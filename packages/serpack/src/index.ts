6f96e444e4af2b4dd1f8cfedeb792166ddf1a518
r.ehdals <kangdongmandoo@gmail.com>
2025-01-12 18:56:31 +0900

    feat: print compilation speed

--- packages/serpack/src/index.ts
+++ packages/serpack/src/index.ts
@@ -1,11 +1,15 @@
+import { debug } from '@serpack/logger';
 import { Compiler, CompilerOptions } from './core';
 
 async function compile(entry: string, compilerOptions?: CompilerOptions) {
+  const now = performance.now();
   const compiler = new Compiler(entry, compilerOptions);
 
   await compiler.compile(entry);
 
   const output = compiler.bundle();
+
+  debug(`Compiled Successfully in ${(performance.now() - now).toFixed()}ms`);
 
   return {
     code: output.code,
