63016bea9fe7f478493aaa2091a58ba22bea0d42
r.ehdals <kangdongmandoo@gmail.com>
2025-01-13 00:42:18 +0900

    fix: fixed infinity loop in `__serpack_require__`

--- packages/serpack/src/core/functions.ts
+++ packages/serpack/src/core/functions.ts
@@ -2,3 +2,4 @@ export const __SERPACK_REQUIRE__ = '__serpack_require__'; // serpack require - u
 export const __NON_SERPACK_REQUIRE__ = 'require'; // non-serpack require
 export const __ESM__ = '__esm__';
 export const __SERPACK_MODULE_CACHE__ = '__serpack_module_cache__';
+export const __SERPACK_MODULE_PENDING__ = '__serpack_module_pending__';
