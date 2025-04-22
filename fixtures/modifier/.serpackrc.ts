c725ad76c3540aed4cd1a4ba04bbadca280dad50
dofmin <dofmin0@naver.com>
2025-04-22 22:43:07 +0900

    feat: caller ast modifier (#13)

---
+++ fixtures/modifier/.serpackrc.ts
@@ -0,0 +1,16 @@
+export default <import('serpack').Options>{
+  compilerOptions: {
+    banner: "const __log=(...args)=>console.log(args.join(''));",
+    modifier: {
+      caller(node) {
+        const wrapCallExpression = {
+          type: 'CallExpression',
+          callee: { type: 'Identifier', name: '__log' },
+          arguments: [node],
+        };
+
+        return wrapCallExpression as any;
+      },
+    },
+  },
+};
---
+++ fixtures/modifier/index.ts
@@ -0,0 +1,6 @@
+function hello(...args: any) {
+  return args;
+}
+
+hello(`${'hello'}`);
+hello(`${'hello2'}`);
